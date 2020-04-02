e5d6d22b1708896dbe0e1b427bade9b65d56a399
André Kovac <me@andrekovac.com>
2020-04-03 00:45:50 +0200

    Connect redux dev tools

--- store/index.ts
+++ store/index.ts
@@ -1,14 +1,19 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import thunk, { ThunkMiddleware } from "redux-thunk";
 
 import { rootReducer, StoreT } from "./reducer";
 import { ActionT } from "./actionCreators";
 
+const devtools =
+  (__DEV__ && global.reduxNativeDevTools) || (() => noop => noop);
+
 const store = createStore(
   rootReducer,
+  compose(
   applyMiddleware(
     thunk as ThunkMiddleware<StoreT, ActionT>
-  )
+  ),
+  devtools()),
 );
 
 export default store;
